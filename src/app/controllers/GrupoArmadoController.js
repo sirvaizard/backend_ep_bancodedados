c164aecf6942add0345835ec23ca7ae23d5494ed
Victor Lopes <thevictorfl@gmail.com>
2020-12-09 20:51:04 -0300

    add: criar grupo armado

---
+++ src/app/controllers/GrupoArmadoController.js
@@ -0,0 +1,25 @@
+import { sql } from '@databases/pg'
+import db from '../../database/index.js'
+
+class GrupoArmadoController {
+  async store(req, res) {
+    const { nome } = req.body
+
+    const groupExists = await db.query(sql`
+      SELECT * FROM grupo_armado WHERE nome = ${nome}
+    `)
+
+    if (groupExists.length > 0)
+      return res.status(400).json({
+        "erro": "Grupo armado com esse nome já existe."
+      })
+
+    const response = await db.query(sql`
+      INSERT INTO grupo_armado (nome) VALUES (${nome})
+    `)
+
+    return res.status(200).json({ "msg": "Grupo armado criado com sucesso" })
+  }
+}
+
+export default new GrupoArmadoController()
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 
 import DivisaoController from './app/controllers/DivisaoController.js'
+import GrupoArmadoController from './app/controllers/GrupoArmadoController.js'
 
 const routes = new Router()
 
 routes.get('/divisao', DivisaoController.index)
+
+routes.post('/grupo_armado', GrupoArmadoController.store)
 
 export default routes
