8654e29fa914fdd66835302152170b0ccb0bbff6
Victor Lopes <thevictorfl@gmail.com>
2020-12-09 23:17:37 -0300

    add: listar numero de conflitos por tipo

--- src/app/controllers/ConflitoController.js
+++ src/app/controllers/ConflitoController.js
@@ -4,6 +4,39 @@ import db from '../../database/index.js'
 const typesConflicts = ['economico', 'racial', 'territorial', 'religioso']
 
 class ConflitoController {
+  async index(req, res) {
+    const racials = await db.query(sql`
+      SELECT COUNT(*) FROM racial
+    `)
+
+    const religious = await db.query(sql`
+      SELECT COUNT(*) FROM religioso
+    `)
+
+    const territorials = await db.query(sql`
+      SELECT COUNT(*) FROM territorial
+    `)
+
+    const economics = await db.query(sql`
+      SELECT COUNT(*) FROM economico
+    `)
+
+    return res.status(200).json({
+      "tipos": [
+        "raciais",
+        "religiosos",
+        "territoriais",
+        "economicos"
+      ],
+      "valores": [
+        racials[0].count,
+        religious[0].count,
+        territorials[0].count,
+        economics[0].count
+      ]
+    })
+  }
+
   async store(req, res) {
     const { nome, num_feridos, num_mortos, tipo } = req.body
 
